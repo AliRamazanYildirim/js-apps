e0e5e042af3a33bc20623f88de77e24f857bca1e
Ali Ramazan Yildirim <ramazan.yildirim@dci-student.org>
2024-09-21 00:15:50 +0200

    Revert "Task-3"
    This reverts commit 36bbdcf433d51196e266abb5323b4e19380eb06b.

--- Uebungen/uebung6.js
+++
@@ -1,9 +0,0 @@
-const readline=require("readline-sync");
-
-//! Task-3
-const startsWithThe = (string) => {
-    return string.toLowerCase().startsWith("the");
-  };
-  const string=readline.question("Gib ein wert ein:");
-  console.log("==========Task-3========");
-  console.log(startsWithThe(string)); 
