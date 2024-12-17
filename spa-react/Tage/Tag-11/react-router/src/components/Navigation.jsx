6e75dbcdce770cb91e3cf0e78f59971126e79c09
Ali Ramazan Yildirim <aliramazanyildirim@gmail.com>
2024-12-17 15:31:42 +0100

    Aktualisiere Navigation-Komponente mit neuen Routen für Home, About und Dashboard

--- spa-react/Tage/Tag-11/react-router/src/components/Navigation.jsx
+++ spa-react/Tage/Tag-11/react-router/src/components/Navigation.jsx
@@ -1,18 +1,20 @@
 import { Link } from "react-router-dom";
 import "../styles/Navigation.module.css";
+import About from "./About";
+import Dashboard from "./Dashboard";
 const Navigation = () => {
   return (
     <header>
       <nav>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/" element={<Home />}>Home</Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
+            <Link to="/about" element={<About />}>About</Link>
           </li>
           <li>
-            <Link to="/dashboard">Dashboard</Link>
+            <Link to="/dashboard" element={<Dashboard />}>Dashboard</Link>
           </li>
         </ul>
       </nav>
