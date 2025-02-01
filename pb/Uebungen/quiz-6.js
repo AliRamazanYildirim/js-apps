c98e1dd05c09f1d82298a51b66c35b66984642ff
Ali Ramazan Yildirim <aliramazanyildirim@gmail.com>
2025-02-01 21:13:07 +0100

    Füge Beispiel für den Vergleich von Referenztypen in JavaScript hinzu

---
+++ pb/Uebungen/quiz-6.js
@@ -0,0 +1,6 @@
+const x = 'hi'.split(',');
+const y = ['h', 'i'];
+console.log(x === y); // false
+
+//Zusammenfassend lässt sich sagen, dass x === y in JavaScript false zurückgibt, auch wenn x und y denselben Inhalt haben, da sie unterschiedliche Referenztypen sind.
+
