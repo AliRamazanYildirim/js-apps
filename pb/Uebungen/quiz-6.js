f9f9ee59c5a1d476632d9b6e50cddca39e9d70f6
Ali Ramazan Yildirim <aliramazanyildirim@gmail.com>
2025-02-02 18:50:33 +0100

    Füge Beispiel für den Post-Inkrementoperator in JavaScript hinzu

--- pb/Uebungen/quiz-6.js
+++ pb/Uebungen/quiz-6.js
@@ -12,3 +12,12 @@ const b = true;
 console.log(a -b); // 6
 
 //In JavaScript wird der Operator - als Subtraktion für Zahlen und als Negation für andere Datentypen verwendet. Da b ein boolescher Wert ist, wird er implizit in eine Zahl umgewandelt. true wird zu 1 und false zu 0. Daher wird true zu 1 und 7 - 1 = 6 zurückgegeben.
+
+BEISPIEL-3
+
+const num = 7;
+
+num = num++;
+console.log(num); // 7
+
+//In JavaScript ist num++ ein Post-Inkrementoperator, der den Wert von num erhöht, nachdem der Wert zurückgegeben wurde. Da num = 7 bereits zugewiesen wurde, wird num++ zu 7 zurückgegeben, bevor num um 1 erhöht wird. Daher bleibt num 7.
