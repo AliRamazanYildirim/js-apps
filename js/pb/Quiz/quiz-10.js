e6fd193186ae3c9b4d8b586717935735abe1607c
Ali Ramazan Yildirim <aliramazanyildirim@gmail.com>
2025-02-17 21:45:36 +0100

    Füge neues Skript hinzu, das die Verwendung der push()-Methode zur Array-Verwaltung demonstriert

---
+++ js/pb/Quiz/quiz-10.js
@@ -0,0 +1,5 @@
+let newList = [1];
+newList.push(2);
+console.log(newList.push(3)); // 3
+
+//Erklärung: Die Methode push() fügt ein oder mehrere Elemente am Ende eines Arrays hinzu und gibt die neue Länge des Arrays zurück. In diesem Fall wird die Zahl 3 hinzugefügt und die Länge des Arrays beträgt nun 3. Die Methode gibt also 3 zurück.
