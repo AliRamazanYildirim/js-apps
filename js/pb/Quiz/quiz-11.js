b640b580031cde521de7f44c232c2b90a0af1511
Ali Ramazan Yildirim <aliramazanyildirim@gmail.com>
2025-02-22 18:10:44 +0100

    Aktualisiere Erklärungen zu logischen Operatoren in quiz-11.js für bessere Verständlichkeit

--- js/pb/Quiz/quiz-11.js
+++ js/pb/Quiz/quiz-11.js
@@ -1,6 +1,6 @@
 console.log(false || null || 'quiz-11.js'); //! quiz-11.js
 console.log(false && null && 'quiz-11.js'); //! false
 
-//? Erklärung: Der logische ODER-Operator (||) gibt den ersten Wert zurück, der als true ausgewertet wird. Wenn alle Werte false sind, wird der letzte Wert zurückgegeben.
+//Erklärung: In JavaScript wird der logische ODER-Operator (||) verwendet, um den ersten wahren Operanden zu finden. Wenn kein wahrer Operand gefunden wird, wird der letzte Operand zurückgegeben. In diesem Fall ist der erste Operand false, daher wird der letzte Operand 'quiz-11.js' zurückgegeben. 
 
-//? Der logische UND-Operator (&&) gibt den ersten Wert zurück, der als false ausgewertet wird. Wenn alle Werte true sind, wird der letzte Wert zurückgegeben. In diesem Fall ist der erste Wert false, daher wird der letzte Wert 'quiz-11.js' zurückgegeben.
+// Der logische UND-Operator (&&) wird verwendet, um den ersten falschen Operanden zu finden. Wenn kein falscher Operand gefunden wird, wird der letzte Operand zurückgegeben. In diesem Fall ist der erste Operand false, daher wird false zurückgegeben.
