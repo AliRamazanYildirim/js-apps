e16715cfe8a1d46cd8e03362fffc35e52649913e
Ali Ramazan Yildirim <aliramazanyildirim@gmail.com>
2025-02-22 18:08:01 +0100

    Füge neues Skript hinzu, das die Verwendung von logischen Operatoren erklärt

---
+++ js/pb/Quiz/quiz-11.js
@@ -0,0 +1,6 @@
+console.log(false || null || 'quiz-11.js'); //! quiz-11.js
+console.log(false && null && 'quiz-11.js'); //! false
+
+//? Erklärung: Der logische ODER-Operator (||) gibt den ersten Wert zurück, der als true ausgewertet wird. Wenn alle Werte false sind, wird der letzte Wert zurückgegeben.
+
+//? Der logische UND-Operator (&&) gibt den ersten Wert zurück, der als false ausgewertet wird. Wenn alle Werte true sind, wird der letzte Wert zurückgegeben. In diesem Fall ist der erste Wert false, daher wird der letzte Wert 'quiz-11.js' zurückgegeben.
