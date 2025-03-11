6c8ff5da6db942f273af3dcd5013d5cc308842b2
Ali Ramazan Yildirim <aliramazanyildirim@gmail.com>
2025-03-11 09:49:09 +0100

    Füge jsonwebtoken-Bibliothek hinzu und entferne nicht mehr benötigte Dateien

---
+++ backend/11.03.2025-user-roles/models/user.js
@@ -0,0 +1,46 @@
+import { Schema, model } from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    roles: {
+      type: [String],
+      enum: ["user", "editor", "admin"],
+      default: ["user"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Schema-Methode zum Hashen des Passworts vor dem Speichern
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+      return next();
+    }
+    try {
+      const salt = await bcrypt.genSalt(10);
+      this.password = await bcrypt.hash(this.password, salt);
+      next();
+    } catch (error) {
+      next(error);
+    }
+  });
+
+const User = model("User", userSchema);
+
+export default User;
