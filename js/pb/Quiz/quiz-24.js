c9e6becfb6186287ca5bcbc97ba7c366071151d1
Ali Ramazan Yildirim <aliramazanyildirim@gmail.com>
2025-03-26 13:21:12 +0100

    Füge Beispiel zur Unveränderlichkeit von Strings in JavaScript hinzu: Erkläre, warum der Versuch, ein Zeichen in einem String zu ändern, nicht funktioniert.

---
+++ js/pb/Quiz/quiz-24.js
@@ -0,0 +1,5 @@
+let a = "Ali";
+a[0]="a";
+console.log(a); // Ali
+
+//Erklärung: Strings sind in JavaScript unveränderlich. Das bedeutet, dass Sie den Wert eines Zeichens in einem String nicht ändern können. Wenn Sie versuchen, den Wert eines Zeichens in einem String zu ändern, wird der Wert nicht geändert. In diesem Fall wird der Wert von a nicht geändert, da Strings in JavaScript unveränderlich sind.
